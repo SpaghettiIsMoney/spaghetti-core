4090f7e6b3a2d0950c18b63d9e808a4ca3ff43e9
spaghettimoney <69653718+spaghettimoney@users.noreply.github.com>
2020-08-17 13:36:03 -0400

    Change file structure for Etherscan verification

--- src/SpaghettiCore.t.sol
+++ src/SpaghettiCore.t.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.5.12;
 
 import "ds-test/test.sol";
 
-import "./SpaghettiCore.sol";
+import "./SpaghettiToken.sol";
+import "./PASTAPool.sol";
 
 interface IUniswapV2Router01 {
     function factory() external pure returns (address);
@@ -97,6 +98,9 @@ interface IUniswapV2Router01 {
     function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
     function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
 }
+interface IUniswapV2Factory {
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+}
 
 contract Hevm {
     function warp(uint256) public;
@@ -104,7 +108,6 @@ contract Hevm {
 }
 
 contract SpaghettiCoreTest is DSTest {
-    SpaghettiFactory core;
     SpaghettiToken token;
     PASTAPool mkrPool;
     PASTAPool wbtcPool;
@@ -116,7 +119,7 @@ contract SpaghettiCoreTest is DSTest {
     PASTAPool yfiPool;
     PASTAPool uniswapPool;
 
-    IERC20 maker = IERC20(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
+    IERC20 mkr = IERC20(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
     IERC20 wbtc = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
     IERC20 comp = IERC20(0xc00e94Cb662C3520282E6f5717214004A7f26888);
     IERC20 lend = IERC20(0x80fB784B7eD66730e8b1DBd9820aFD29931aab03);
@@ -125,8 +128,9 @@ contract SpaghettiCoreTest is DSTest {
     IERC20 link = IERC20(0x29E240CFD7946BA20895a7a02eDb25C210f9f324);
     IERC20 yfi = IERC20(0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e);
     IERC20 yycrv = IERC20(0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c);
-    IERC20 univ2;
+    address univ2;
 
+    IUniswapV2Factory uniswapFactory = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);    
     IUniswapV2Router01 router = IUniswapV2Router01(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
 
     Hevm hevm;
@@ -136,51 +140,94 @@ contract SpaghettiCoreTest is DSTest {
 
     function setUp() public {
         hevm = Hevm(address(CHEAT_CODE));
-        core = new SpaghettiFactory();
-        core.initMKR();
-        core.initCOMP();
-        core.initLINK();
-        core.initSNX();
-        core.initYFI();
-        core.initLEND();
-        core.initWETH();
-        core.initWBTC();
-        core.initUNI();
-        univ2 = IERC20(core.uniswap());
-        token = core.spaghetti();
-        mkrPool = core.mkrPool();
-        wbtcPool = core.wbtcPool();
-        compPool = core.compPool();
-        lendPool = core.lendPool();
-        snxPool = core.snxPool();
-        wethPool = core.wethPool();
-        linkPool = core.linkPool();
-        yfiPool = core.yfiPool();
-        uniswapPool = core.uniswapPool();
+        token = new SpaghettiToken(address(this));
+        mkrPool = new PASTAPool(address(token), address(mkr));
+        token.transfer(address(mkrPool), 1000000000000000000000000);
+        mkrPool.setRewardDistribution(address(this));
+        mkrPool.notifyRewardAmount(990000000000000000000000);
+        mkrPool.setRewardDistribution(address(0));
+        mkrPool.renounceOwnership();
+
+        wbtcPool = new PASTAPool(address(token), address(wbtc));
+        token.transfer(address(wbtcPool), 1000000000000000000000000);
+        wbtcPool.setRewardDistribution(address(this));
+        wbtcPool.notifyRewardAmount(990000000000000000000000);
+        wbtcPool.setRewardDistribution(address(0));
+        wbtcPool.renounceOwnership();
+
+        compPool = new PASTAPool(address(token), address(comp));
+        token.transfer(address(compPool), 1000000000000000000000000);
+        compPool.setRewardDistribution(address(this));
+        compPool.notifyRewardAmount(990000000000000000000000);
+        compPool.setRewardDistribution(address(0));
+        compPool.renounceOwnership();
+
+        lendPool = new PASTAPool(address(token), address(lend));
+        token.transfer(address(lendPool), 1000000000000000000000000);
+        lendPool.setRewardDistribution(address(this));
+        lendPool.notifyRewardAmount(990000000000000000000000);
+        lendPool.setRewardDistribution(address(0));
+        lendPool.renounceOwnership();
+
+        snxPool = new PASTAPool(address(token), address(snx));
+        token.transfer(address(snxPool), 1000000000000000000000000);
+        snxPool.setRewardDistribution(address(this));
+        snxPool.notifyRewardAmount(990000000000000000000000);
+        snxPool.setRewardDistribution(address(0));
+        snxPool.renounceOwnership();
+
+        wethPool = new PASTAPool(address(token), address(weth));
+        token.transfer(address(wethPool), 1000000000000000000000000);
+        wethPool.setRewardDistribution(address(this));
+        wethPool.notifyRewardAmount(990000000000000000000000);
+        wethPool.setRewardDistribution(address(0));
+        wethPool.renounceOwnership();
+
+        linkPool = new PASTAPool(address(token), address(link));
+        token.transfer(address(linkPool), 1000000000000000000000000);
+        linkPool.setRewardDistribution(address(this));
+        linkPool.notifyRewardAmount(990000000000000000000000);
+        linkPool.setRewardDistribution(address(0));
+        linkPool.renounceOwnership();
+
+        yfiPool = new PASTAPool(address(token), address(yfi));
+        token.transfer(address(yfiPool), 1000000000000000000000000);
+        yfiPool.setRewardDistribution(address(this));
+        yfiPool.notifyRewardAmount(990000000000000000000000);
+        yfiPool.setRewardDistribution(address(0));
+        yfiPool.renounceOwnership();
+
+        univ2 = uniswapFactory.createPair(0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c, address(token));
+        uniswapPool = new PASTAPool(address(token), univ2);
+        uniswapPool.setRewardDistribution(address(this));
+        token.transfer(address(uniswapPool), 7000000000000000000000000);
+        uniswapPool.notifyRewardAmount(6930000000000000000000000);
+        uniswapPool.setRewardDistribution(address(0));
+        uniswapPool.renounceOwnership();
     }
 
     function test_mkr() public {
         hevm.store(
-            address(maker),
+            address(mkr),
             keccak256(abi.encode(address(this), uint256(1))),
             bytes32(uint256(999999999999 ether))
         );
-        hevm.warp(now + 24 hours);
-        maker.approve(address(mkrPool), uint256(-1));
+        hevm.warp(1597777200);
+        mkr.approve(address(mkrPool), uint256(-1));
         mkrPool.stake(1 ether);
         hevm.warp(now + 10 days);
         mkrPool.exit();
         assertEq(token.balanceOf(address(this)),  980099999999999999543808);
-        assertEq(maker.balanceOf(address(this)), 999999999999 ether);
+        assertEq(mkr.balanceOf(address(this)), 999999999999 ether);
     }
 
     function testFail_mkr_too_early() public {
         hevm.store(
-            address(maker),
+            address(mkr),
             keccak256(abi.encode(address(this), uint256(1))),
             bytes32(uint256(999999999999 ether))
         );
-        maker.approve(address(mkrPool), uint256(-1));
+        mkr.approve(address(mkrPool), uint256(-1));
         mkrPool.stake(1 ether);
     }
 
@@ -193,23 +240,13 @@ contract SpaghettiCoreTest is DSTest {
         );
         assertEq(wbtc.balanceOf(address(this)), 999999999999 ether);
 
-        hevm.warp(now + 24 hours);
+        hevm.warp(1597777200);
         wbtc.approve(address(wbtcPool), uint256(-1));
         wbtcPool.stake(1 ether);
         hevm.warp(now + 10 days);
         wbtcPool.exit();
         assertEq(token.balanceOf(address(this)),  980099999999999999543808);
         assertEq(wbtc.balanceOf(address(this)), 999999999999 ether);
-    }
-
-    function testFail_wbtc_too_early() public {
-        hevm.store(
-            address(wbtc),
-            keccak256(abi.encode(address(this), uint256(0))),
-            bytes32(uint256(999999999999 ether))
-        );
-        wbtc.approve(address(wbtcPool), uint256(-1));
-        wbtcPool.stake(1 ether);
     }
 
     function test_uni() public {
@@ -231,19 +268,19 @@ contract SpaghettiCoreTest is DSTest {
         uint exp = block.timestamp + 1 days;
         router.addLiquidity(address(yycrv), address(token), 999999 ether, 999999 ether, 999999 ether, 999999 ether, address(this), exp);
 
-        assertEq(univ2.balanceOf(address(this)), 994986442119182848113525);
+        assertEq(IERC20(univ2).balanceOf(address(this)), 994986442119182848113525);
 
-        hevm.warp(now + 48 hours);
-        univ2.approve(address(uniswapPool), uint256(-1));
+        hevm.warp(1597777200);
+        IERC20(univ2).approve(address(uniswapPool), uint256(-1));
         uniswapPool.stake(1 ether);
         hevm.warp(now + 26 days);
         uniswapPool.exit();
-        assertEq(token.balanceOf(address(this)),  6860699999999999999201664);
-        assertEq(univ2.balanceOf(address(this)), 994986442119182848113525);
+        assertEq(token.balanceOf(address(this)),  6860699999999999999800416);
+        assertEq(IERC20(univ2).balanceOf(address(this)), 994986442119182848113525);
 
-        univ2.approve(address(router), uint(-1));
+        IERC20(univ2).approve(address(router), uint(-1));
         exp = block.timestamp + 1 days;
-        router.removeLiquidity(address(yycrv), address(token), univ2.balanceOf(address(this)), 0, 0, address(this), exp);
+        router.removeLiquidity(address(yycrv), address(token), IERC20(univ2).balanceOf(address(this)), 0, 0, address(this), exp);
     }
 
 }
